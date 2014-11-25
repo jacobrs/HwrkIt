033bcd2598e856435b140d9d195d19575a21a932
shanesteszyn <ssteszyn@gmail.com>
2014-11-25 13:48:38 -0500

    Bens spelling

--- Server/src/main/java/app/UserRepository.java
+++ Server/src/main/java/app/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "users", path = "userse")
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
     List<User> findByLastName(@Param("name") String name);
