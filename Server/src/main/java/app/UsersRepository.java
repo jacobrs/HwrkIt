50ab657123e6da04389701e3a09766b1d46a20e5
shanesteszyn <ssteszyn@gmail.com>
2014-11-26 11:28:27 -0500

    Users db.

--- Server/src/main/java/app/UsersRepository.java
+++ Server/src/main/java/app/UsersRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "users", path = "users")
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+@RepositoryRestResource(collectionResourceRel = "Users", path = "Users")
+public interface UsersRepository extends PagingAndSortingRepository<User, Long> {
 
     List<User> findByLastName(@Param("name") String name);
 
