c5e9d9297db62737e8a1839396222e4634c270e7
shanesteszyn <ssteszyn@gmail.com>
2014-11-26 13:44:52 -0500

    CrudRepo

--- Server/src/main/java/app/UsersRepository.java
+++ Server/src/main/java/app/UsersRepository.java
@@ -2,12 +2,12 @@ package app;
 
 import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "Users", path = "Users")
-public interface UsersRepository extends PagingAndSortingRepository<User, Long> {
+public interface UsersRepository extends CrudRepository<User, Long> {
 
     List<User> findByLastName(@Param("name") String name);
 
